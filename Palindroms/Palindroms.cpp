86b10142fd97678e62f0e968e9842300ea2bf499
Anna <anna130197@gmail.com>
2018-04-08 17:41:24 +0200

    Update Palindroms.cpp

--- Palindroms/Palindroms.cpp
+++ Palindroms/Palindroms.cpp
@@ -8,10 +8,10 @@
 void findPalindrome(char*);
 bool checkIfNines(char*);
 void handleSameNines(char*);
-void addOne(char*, int);
+void addOne(char*, unsigned int);
 void findPalindromeNumber(char*);
 
-const int BUFFORSIZE = 1000000;
+const unsigned int BUFFORSIZE = 1000000;
 
 int main()
 {
@@ -38,7 +38,7 @@ int main()
 
 //Put '1' on the beginning and end(one position in right to the previous length). Put zeros in the middle.
 void handleSameNines(char *number) { 
-	int length = strlen(number);
+	unsigned int length = strlen(number);
 
 	number[length + 1] = number[length];
 	number[length] = '1';
@@ -49,22 +49,25 @@ void handleSameNines(char *number) {
 }
 
 bool checkIfNines(char *number) {
-	int i = 0;
+	unsigned int i = 0;
 
-	while (*(number + i) != '\0') 
-		if (*(number + i++) != '9')
+	while (*(number + i) != '\0') {
+		if (*(number + i++) != '9') {
 			return false;
+		}
+	}
 	
 	return true;
 }
 
-void addOne(char *number, int positionToAddOne) {
+void addOne(char *number, unsigned int positionToAddOne) {
 	++number[positionToAddOne];
 	while (number[positionToAddOne] == 58 && positionToAddOne >= 0) {
 		number[positionToAddOne] = '0';
 		if (positionToAddOne == 0) { //Move all digits 1 position in right, add '1' on beginning.
-			for (int i = strlen(number) + 1; i > 0; --i)
+			for (int i = strlen(number) + 1; i > 0; --i) {
 				number[i] = number[i - 1];
+			}
 			number[0] = '1';
 		}
 		++number[--positionToAddOne];
@@ -72,24 +75,26 @@ void addOne(char *number, int positionToAddOne) {
 }
 
 void findPalindromeNumber(char* number) {
-	const int length = strlen(number);
-	const int centerOfSymmetry = (length + 1) / 2;
-	int digitPostitionFromEnd = length - 1;
-	int digitPostitionFromBeginning = 0;
+	const unsigned int length = strlen(number);
+	const unsigned int centerOfSymmetry = (length + 1) / 2;
+	unsigned int digitPostitionFromEnd = length - 1;
+	unsigned int digitPostitionFromBeginning = 0;
 	
 	//Add 1 in order to prevent accept input palindrome as result palindrome.
 	addOne(number, digitPostitionFromEnd);
 	
 	while (digitPostitionFromBeginning < centerOfSymmetry) {
-		if (number[digitPostitionFromBeginning] < number[digitPostitionFromEnd]) 
+		if (number[digitPostitionFromBeginning] < number[digitPostitionFromEnd]) {
 			addOne(number, digitPostitionFromEnd - 1);
+		}
 		number[digitPostitionFromEnd--] = number[digitPostitionFromBeginning++];
 	}
 }
 
 void findPalindrome(char* number) {
-	if (checkIfNines(number)) 
+	if (checkIfNines(number)) {
 		handleSameNines(number);
-	else
+	} else {
 		findPalindromeNumber(number);
+	}
 }
